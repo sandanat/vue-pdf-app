c6a47586cdb96876fa24aea665eb985e1c446757
sandanat <sanat62@yandex.ru>
2020-08-06 23:46:27 +0300

    Add l10n.js in  src/pdfjs-dist/lib/external/webL10n/l10n.js

--- src/components/pdf-viewer.vue
+++ src/components/pdf-viewer.vue
@@ -4,13 +4,18 @@
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
-import "pdfjs-dist/es5/build/pdf";
-import * as pdfApp from "pdfjs-dist/lib/web/app";
-import "pdfjs-dist/lib/web/genericcom";
-import "pdfjs-dist/lib/web/pdf_print_service";
-import "pdfjs-dist/build/pdf.worker.entry";
+import "@/pdfjs-dist/es5/build/pdf";
+import * as pdfApp from "@/pdfjs-dist/lib/web/app";
+import { AppOptions } from "@/pdfjs-dist/lib/web/app_options";
+import "@/pdfjs-dist/lib/web/genericcom";
+import "@/pdfjs-dist/lib/web/pdf_print_service";
+import "@/pdfjs-dist/build/pdf.worker.entry";
 import "../css/index.css";
 
+if (AppOptions) {
+  AppOptions.set("defaultUrl", null);
+  AppOptions.set("locale", "ru");
+}
 
 @Component
 export default class HelloWorld extends Vue {
