411dbe598977ed125b52350dd9254d8f554f1451
sandanat <sanat62@yandex.ru>
2020-08-13 00:38:31 +0300

    Configure webpack to load images in css

--- src/App.vue
+++ src/App.vue
@@ -15,14 +15,3 @@ import PDF from './components/pdf-viewer.vue';
 })
 export default class App extends Vue {}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   lintOnSave: false,
-  transpileDependencies: ["pdfjs-dist"]
-}
+  transpileDependencies: ["pdfjs-dist"],
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.(cur)$/,
+          use: ["file-loader"]
+        }
+      ]
+    }
+  }
+};
