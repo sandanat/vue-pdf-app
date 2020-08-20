358323fe3ea7d0f9b388cfcdf9b6f9ecfcc376d5
sandanat <sanat62@yandex.ru>
2020-08-20 16:35:48 +0300

    Add lazy loading example

---
+++ examples/lazy-loading/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <pdf-viewer pdf="/sample.pdf"></pdf-viewer>
+  </div>
+</template>
+
+<script>
+import Loader from "./components/Loader.vue";
+
+export default {
+  name: "App",
+  components: {
+    "pdf-viewer": () => ({
+      component: new Promise((res) => {
+        return setTimeout(
+          () => res(import(/* webpackChunkName: "pdf-viewer" */ "vue-pdf-app")),
+          4000
+        );
+      }),
+      loading: Loader,
+    }),
+  },
+};
+</script>
+
+<style>
+#app {
+  height: 100%;
+  text-align: center;
+}
+
+#app h3 {
+  margin-top: 30px;
+}
+</style>
---
+++ examples/lazy-loading/src/components/Loader.vue
@@ -0,0 +1,7 @@
+<template>
+  <h3>Component is loading...</h3>
+</template>
+
+<script>
+export default {};
+</script>
