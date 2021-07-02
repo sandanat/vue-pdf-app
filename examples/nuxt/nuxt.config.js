2eadcbf16a775b67754105972b3938130ee92271
sandanat <sanat62@yandex.ru>
2021-07-02 11:51:13 +0300

    Created nuxt example

--- examples/nuxt/nuxt.config.js
+++ examples/nuxt/nuxt.config.js
@@ -25,6 +25,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    "~/plugins/vuePdfApp.js"
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
